2a428426bd06d743fa577e728c2a224c0fc4dc8c
Faizan2007 <73649374+Faizan2007@users.noreply.github.com>
2022-01-26 12:51:19 +0300

    Added Files

---
+++ README.md
@@ -0,0 +1,51 @@
+<h3 style="font-size:35px;">🏆 Pokétwo Autocatcher 🏆</h3>
+A <b>free</b> and open-source Pokétwo autocatcher, created with the goal of preventing people from wasting their money.
+
+---
+
+### Features
+The bot has the following features:
+- ⚙️ Easy Setup (simply run a script and enter the correct information, no 'programming' required)
+- ⬆️ Auto-levelling (Level up all your duel Pokémon to level 100 **overnight**!)
+- ✨ Get notified when and if you've caught a Pokémon, and also if another event occurs
+    - See when a Shiny/Legendary/Ultra Beast/Mythical Pokémon is caught, and which one it is!
+- 💲 **Completely free** and open-source (you can see the code as it is currently)
+- 💕 **Trustworthy**; this autocatcher is **completely** open-source (meaning you can see the latest code)
+- 📜 Support for all Pokémon from generation I to generation VIII, including Alolan and Galarian Pokémon
+
+### Requirements
+Please note that this autocatcher requires Python 3.8 installed. Python 3.9 won't work with Discord.
+
+### Getting Started:
+To start up the bot for the first time, please install the required dependencies first:
+```
+python -m pip install -U git+https://github.com/Merubokkusu/Discord-S.C.U.M.git
+```
+> Remember to modify the command to your installed one if necessary. <br>
+> However, this command should work in most cases.
+
+#### <b>Running the bot</b>
+Once you have installed the correct dependencies, run the `setup.py` file and enter in the following fields when asked.
+
+#### <b>user_token</b>:
+Paste in your discord account's user token. You can find instructions on this [here](https://www.youtube.com/watch?v=3W9tAEsK7RM) <br>
+
+#### <b>channel_id</b>:
+This will allow the bot to use your preferred channel to spam as well as catch Pokétwo spawns. <br>
+Make sure you paste this carefully, as If you set it to the wrong channel it will spam and catch there. <br>
+See [here](https://www.youtube.com/watch?v=6dqYctHmazc) for help on getting this.
+
+After you've done the above, simply run the following command to start up your very own Pokétwo Autocatcher.
+```
+py main.py
+```
+The command may vary depending on your operating system, so try `python3 main.py` if this doesn't work.
+
+> Remember to cd into your autocatcher folder as well. If you need any help with something, feel free to open a Github Issue.
+
+### Auto-levelling
+To enable auto-levelling, just put in the ID's of the Pokémon you want to be levelled up into the `level.txt` text file.
+
+<h3 style="color:#f76767;">⚠️<b>Disclaimer</b>⚠️</h3> 
+Please note that self botting is against Discord's Terms of Service and being discovered <br> 
+using a self bot may result in your account being banned. To avoid this, keep knowledge of your self bot <br> to a minimum and use a throwaway account. I am not responsible for any accounts lost due to the self bot. <br> I also recommend checking the self bot channel's messages occasionally to see if Pokétwo has sent a captcha. <br> <b>If it has, it would be a good idea to solve it.</b> <br>
---
+++ main.py
@@ -0,0 +1,139 @@
+import discum, re, time, multiprocessing, json, datetime, random, keep_alive
+
+version = 'v2.6'
+
+with open('data/config.json','r') as file:
+    info = json.loads(file.read())
+    user_token = info['user_token']
+    channel_id = info['channel_id']
+
+with open('data/pokemon.txt', 'r', encoding='utf8') as file:
+    pokemon_list = file.read()
+with open('data/legendary.txt','r') as file:
+    legendary_list = file.read()
+with open('data/mythical.txt','r') as file:
+    mythical_list = file.read()
+with open('data/level.txt','r') as file:
+    to_level = file.readline()
+
+num_pokemon = 0
+shiny = 0
+legendary = 0
+mythical = 0
+
+poketwo_id = '716390085896962058'
+bot = discum.Client(token=user_token, log=False)
+keep_alive.keep_alive()
+
+def solve(message):
+    hint = []
+    for i in range(15, len(message) - 1):
+        if message[i] != '\\':
+            hint.append(message[i])
+    hint_string = ''
+    for i in hint:
+        hint_string += i
+    solution = re.findall('^'+hint_string.replace('_', '.')+'$', pokemon_list, re.MULTILINE)
+    return solution
+
+def spam():
+  while True:
+    num = random.randint(1, 1000000000000)
+    bot.sendMessage(channel_id, num)
+    intervals = [2.0,2.1,2.2,2.3,2.4,2.5]
+    time.sleep(random.choice(intervals))
+
+def start_spam():
+    new_process = multiprocessing.Process(target=spam)
+    new_process.start()
+    return new_process
+
+def stop(process):
+    process.terminate()
+
+def log(string):
+    now = datetime.datetime.now()
+    current_time = now.strftime('%H:%M:%S')
+    print(f'[{current_time}]', string)
+
+@bot.gateway.command
+def on_ready(resp):
+    if resp.event.ready_supplemental:
+        user = bot.gateway.session.user
+        log(f'Logged into account: {user["username"]}#{user["discriminator"]}')
+
+@bot.gateway.command
+def on_message(resp):
+    global spam_process
+    if resp.event.message:
+        m = resp.parsed.auto()
+        if m['channel_id'] == channel_id:
+            if m['author']['id'] == poketwo_id:
+                if m['embeds']:
+                    embed_title = m['embeds'][0]['title']
+                    if 'wild pokémon has appeared!' in embed_title:
+                        stop(spam_process)
+                        time.sleep(2)
+                        bot.sendMessage(channel_id, 'p!h')
+                    elif "Congratulations" in embed_title:
+                        embed_content = m['embeds'][0]['description']
+                        if 'now level' in embed_content:
+                            stop(spam_process)
+                            split = embed_content.split(' ')
+                            a = embed_content.count(' ')
+                            level = int(split[a].replace('!', ''))
+                            if level == 100:
+                                bot.sendMessage(channel_id, f"p!s {to_level}")
+                                with open('data/level.txt', 'r') as fi:
+                                    data = fi.read().splitlines(True)
+                                with open('data/level.txt', 'w') as fo:
+                                    fo.writelines(data[1:])
+                                spam_process = start_spam()
+                            else:
+                                spam_process = start_spam()
+                else:
+                    content = m['content']
+                    if 'The pokémon is ' in content:
+                        if len(solve(content)) == 0:
+                            log('Pokemon not found.')
+                        else:
+                            for i in solve(content):
+                                stop(spam_process)
+                                time.sleep(2)
+                                bot.sendMessage(channel_id, f'p!c {i}')
+                        time.sleep(2)
+                        spam_process = start_spam()
+
+                    elif 'Congratulations' in content:
+                        global shiny
+                        global legendary
+                        global num_pokemon
+                        global mythical
+                        num_pokemon += 1
+                        split = content.split(' ')
+                        pokemon = split[7].replace('!','')
+                        if 'These colors seem unusual...' in content:
+                            shiny += 1
+                            log(f'A shiny Pokémon was caught! Pokémon: {pokemon}')
+                            log(f'Shiny: {shiny} | Legendary: {legendary} | Mythical: {mythical}')
+                        elif re.findall('^'+pokemon+'$', legendary_list, re.MULTILINE):
+                            legendary += 1
+                            log(f'A legendary Pokémon was caught! Pokémon: {pokemon}')
+                            log(f'Shiny: {shiny} | Legendary: {legendary} | Mythical: {mythical}')
+                        elif re.findall('^'+pokemon+'$', mythical_list, re.MULTILINE):
+                            mythical += 1
+                            log(f'A mythical Pokémon was caught! Pokémon: {pokemon}')
+                            log(f'Shiny: {shiny} | Legendary: {legendary} | Mythical: {mythical}')
+                        else:
+                            print(f'Total Pokémon Caught: {num_pokemon}')
+
+                    elif 'human' in content:
+                        stop(spam_process)
+                        log('Captcha Detected; Autocatcher Paused. Press enter to restart.')
+                        input()
+                        bot.sendMessage(channel_id, 'p!h')
+
+if __name__ == '__main__':
+    print(f'Pokétwo Autocatcher {version}\nA FOSS Pokétwo autocatcher\nEvent Log:')
+    spam_process = start_spam()
+    bot.gateway.run(auto_reconnect=True)
